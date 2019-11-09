9666b4d12517d9ba3b0dffac37d4133ef5e48966
Ruchir Agrawal <masterruchir.18@gmail.com>
2019-11-09 19:30:26 +0530

    Init Commit

---
+++ src/main/java/s2/devops/s2devops/S2DevopsApplication.java
@@ -0,0 +1,13 @@
+package s2.devops.s2devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2DevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s2/devops/s2devops/S2DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package s2.devops.s2devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
