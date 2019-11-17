58a5339a5f95f4db66a281f0bbe466f33659ef12
Ruchir Agrawal <masterruchir.18@gmail.com>
2019-11-18 00:45:45 +0530

    chngd per on ec2

--- runapp.sh
+++ runapp.sh
@@ -1,4 +1,4 @@
 echo 'Starting my app'
 cd /home/ec2-user/cd-demo/target
 echo 'directory changed to target location'
-java -jar s2-devops-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-user/cd-demo/target/s2-devops-0.0.1-SNAPSHOT.jar
