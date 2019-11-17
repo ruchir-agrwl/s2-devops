651b4c829ab9c20d0dc20d116c9111bc1e2ec9e3
Ruchir Agrawal <masterruchir.18@gmail.com>
2019-11-17 23:59:45 +0530

    cp run

--- src/main/java/s2/devops/s2devops/S2DevopsApplication.java
+++ src/main/java/s2/devops/s2devops/S2DevopsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class S2DevopsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(S2DevopsApplication.class, args);
 	}
-
 }
