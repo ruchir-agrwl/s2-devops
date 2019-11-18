e06c70d00bad96cbaf1e8a61dd92a2e9b45167bb
Ruchir Agrawal <masterruchir.18@gmail.com>
2019-11-18 11:00:59 +0530

    added validation step in appspec.yml

--- runapp.sh
+++ runapp.sh
@@ -1,4 +1,6 @@
 echo 'Starting my app'
 cd /home/ec2-user/cd-demo/target
+chmod +x /home/ec2-user/cd-demo/target
+echo 'permissions updated'
 echo 'directory changed to target location'
 java -jar /home/ec2-user/cd-demo/target/s2-devops-0.0.1-SNAPSHOT.jar
---
+++ validateapp.sh
@@ -0,0 +1,2 @@
+echo 'validating my app'
+curl localhost:8001/s2-cicd/devops
