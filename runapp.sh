020bb7f17d9657f62e79c5864df3d2fea2638e57
Ruchir Agrawal <masterruchir.18@gmail.com>
2019-12-10 00:41:33 +0530

    permisson command updated

--- runapp.sh
+++ runapp.sh
@@ -1,6 +1,6 @@
 echo 'Starting my app'
 cd /home/ec2-user/cd-demo/target
-chmod a+x /home/ec2-user/cd-demo/target
+chmod +x /home/ec2-user/cd-demo/target
 echo 'permissions updated'
 echo 'directory changed to target location'
 java -jar s2-devops-0.0.1-SNAPSHOT.jar  >java-app.txt 2>&1 &
